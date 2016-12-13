1cfc44e0453215f11a5f31a9e8d957d57cdd926a
lcs <lcs@lcss-mac.local>
2016-12-12 21:42:45 -0800

    Initial Commit

---
+++ FootingCalculatorPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FootingCalculatorPro
+//
+//  Created by lcs on 12/12/16.
+//  Copyright © 2016 lcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
